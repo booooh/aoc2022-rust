8f1a88c8202a7ca80950981fa85bf2171b7f0799
booooh <ben.dayan@gmail.com>
2022-12-03 23:02:06 +0200

    feat: extract parsing logic to common crate

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::File,
-    io::{self, BufRead},
-    path::Path,
-};
+use common::{Parsable, ParseStatus};
 
 #[derive(Default, Debug)]
 struct Elf {
@@ -15,44 +11,6 @@ impl Elf {
     }
     fn total_calories(&self) -> i64 {
         self.inventory.iter().sum::<i64>()
-    }
-}
-
-enum ParseStatus {
-    ItemComplete,
-    ItemIncomplete,
-}
-
-trait Parsable: Sized {
-    fn parse(lines: io::Lines<io::BufReader<File>>) -> Vec<Self> {
-        let mut items = Vec::new();
-        let mut curr_item = None;
-        for line in lines {
-            if let Ok(line_val) = line {
-                let parse_res = Self::parse_line(&line_val, &mut curr_item);
-                curr_item = match parse_res {
-                    ParseStatus::ItemComplete => {
-                        items.push(curr_item.unwrap());
-                        None
-                    }
-                    ParseStatus::ItemIncomplete => curr_item,
-                }
-            }
-        }
-        // add the last item
-        if let Some(item) = curr_item {
-            items.push(item);
-        }
-
-        return items;
-    }
-    fn parse_line(line: &str, curr_item: &mut Option<Self>) -> ParseStatus;
-    fn parse_file<PathType>(filename: PathType) -> Vec<Self>
-    where
-        PathType: AsRef<Path>,
-    {
-        let file = File::open(filename).unwrap();
-        Self::parse(io::BufReader::new(file).lines())
     }
 }
 
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,8 +1,4 @@
-use std::{
-    fs::File,
-    io::{self, BufRead},
-    path::Path,
-};
+use common::{Parsable, ParseStatus};
 
 #[derive(Debug)]
 enum Choice {
@@ -72,44 +68,6 @@ impl Round {
                 Choice::Scissors => RoundResult::Draw,
             },
         }
-    }
-}
-
-enum ParseStatus {
-    ItemComplete,
-    ItemIncomplete,
-}
-
-trait Parsable: Sized {
-    fn parse(lines: io::Lines<io::BufReader<File>>) -> Vec<Self> {
-        let mut items = Vec::new();
-        let mut curr_item = None;
-        for line in lines {
-            if let Ok(line_val) = line {
-                let parse_res = Self::parse_line(&line_val, &mut curr_item);
-                curr_item = match parse_res {
-                    ParseStatus::ItemComplete => {
-                        items.push(curr_item.unwrap());
-                        None
-                    }
-                    ParseStatus::ItemIncomplete => curr_item,
-                }
-            }
-        }
-        // add the last item
-        if let Some(item) = curr_item {
-            items.push(item);
-        }
-
-        return items;
-    }
-    fn parse_line(line: &str, curr_item: &mut Option<Self>) -> ParseStatus;
-    fn parse_file<PathType>(filename: PathType) -> Vec<Self>
-    where
-        PathType: AsRef<Path>,
-    {
-        let file = File::open(filename).unwrap();
-        Self::parse(io::BufReader::new(file).lines())
     }
 }
 
