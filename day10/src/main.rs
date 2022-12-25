894967eb4fae8b751bc6ce4062a38074a3ec8854
booooh <ben.dayan@gmail.com>
2022-12-25 09:11:07 +0200

    feat: day10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -23,15 +23,42 @@ struct Program {
     instructions: VecDeque<Instruction>,
     curr_value: isize,
     history_values: Vec<isize>,
+    crt: [char; 240],
 }
 
 impl Program {
     fn new(instructions: &VecDeque<Instruction>) -> Self {
-        return Self {
+        let crt = ['.'; 240];
+        let mut res = Self {
             instructions: instructions.clone(),
             curr_value: 1,
             history_values: vec![1],
+            crt,
         };
+
+        res.update_crt();
+
+        return res;
+    }
+
+    fn update_crt(&mut self) {
+        // update the crt
+        let curr_cycle = self.history_values.len();
+        let drawn_pixel = ((curr_cycle - 1) % 40) as isize;
+
+        let sprite_position = self.curr_value - 1..self.curr_value + 2;
+        dbg!(&curr_cycle, &drawn_pixel, &sprite_position);
+
+        // if the pixel being drawn is within the sprite position
+        if sprite_position.contains(&drawn_pixel) {
+            self.crt[curr_cycle - 1] = '#'
+        }
+    }
+
+    fn display(&self) {
+        for row in 0..6 {
+            println!("{}", String::from_iter(&self.crt[row * 40..(row + 1) * 40]));
+        }
     }
 
     fn run_instruction(&mut self) {
@@ -40,12 +67,15 @@ impl Program {
             Instruction::NOOP => {
                 // the value remains the same, so record that and increment the cycle count
                 self.history_values.push(self.curr_value);
+                self.update_crt()
             }
             Instruction::ADDX(value) => {
                 // the value remains the same for one cycle, and then incremented
                 self.history_values.push(self.curr_value);
+                self.update_crt();
                 self.curr_value += value;
                 self.history_values.push(self.curr_value);
+                self.update_crt();
             }
         }
     }
@@ -69,6 +99,9 @@ fn main() {
     while !program.instructions.is_empty() {
         program.run_instruction();
     }
+
+    // dbg!(program.crt);
+    program.display();
 
     let val = program
         .history_values
